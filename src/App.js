b84c5b8e69fc89d164afbf5346fc8609f33a4147
Thomas Gustafsson <63698849+Gustho-dot@users.noreply.github.com>
2021-05-21 21:09:19 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './routes/home';
+import About from './routes/about'
+import Contact from './routes/contact'
+
+function App() {
+  return (
+    <div className="App">
+          <Router>
+          <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/About">
+                      <About />
+                    </Route>
+                    <Route path="/Contact">
+                      <Contact />
+                    </Route>
+                </Switch>
+            
+        </Router>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
