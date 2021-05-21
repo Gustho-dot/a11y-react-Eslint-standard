31bde73d7a63db7bb1143bba8c2a8bcc3ceae059
Thomas Gustafsson <63698849+Gustho-dot@users.noreply.github.com>
2021-05-21 21:28:53 +0200

    Add files via upload

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div class="inputWrapper">
+        <h1>Contact Page</h1>
+
+            <section class="inputSection">
+            <input type="text" placeholder="First name.." />
+            <input type="text" placeholder="Last name.." />
+            <input type="email" placeholder="Type your e-mail.." />
+            <button class="submitButton">Submit</button>
+            </section>
         </div>
     )
 }
